6931a370c011e9b081f5ae22ade300a6479d7e1c
Jonathan McCaffrey <jon.mcc_social@live.com>
2011-12-19 19:20:44 +0000

    Starting on dynamic directory file loader.
    git-svn-id: https://subversion.assembla.com/svn/super-platformer/trunk@33 2a89a94e-e93b-44f9-8cd7-c7926ceb498e

---
+++ Super Platformer/Button/Button/Files/FileListLoader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Button
+{
+    //<summary>
+    // FileListLoader objects take a filepath to a directory.
+    // Upon calling the Load function which takes no paramters and return void, 
+    // they load all values from the directory.
+    // These files paths can then be grabbed from the getter, ListOfFilePaths.
+    //</summary>
+    public class FileListLoader
+    {
+        private string mFilePathToDirectory = null;
+
+        public string FilePathToDirectory
+        {
+            get {
+                if (mFilePathToDirectory == null)
+                {
+                    throw new Exception(this.ToString() + "Null filepath\n\r");
+                }
+
+                return mFilePathToDirectory; }
+        }
+        private List<string> mListOfFilePaths = new List<string>();
+
+        public List<string> ListOfFilePaths
+        {
+            get { return mListOfFilePaths; }
+        }
+
+        FileListLoader(string aFilePathToDirectory)
+        {
+            mFilePathToDirectory = aFilePathToDirectory;
+        }
+
+        void Load()
+        {
+
+        }
+
+
+        public override string ToString()
+        {
+            return "FileListLoader.cs";
+        }
+    }
+}
